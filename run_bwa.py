1c1a9920ae3536051b4f9426dd4b20f4b185a04b
Tyghe Vallard <vallardt@gmail.com>
2013-12-19 15:49:09 -0500

    bwa_mem seems to be working

--- run_bwa.py
+++ run_bwa.py
@@ -1,7 +1,12 @@
 from bwa import seqio
+from bwa.bwa import BWAMem, index_ref, which_bwa
+#import bwa.bwa
 
 import os
 from os.path import *
+import logging
+
+log = logging.getLogger(__name__)
 
 class InvalidReadFile(Exception): pass
 
@@ -53,3 +58,38 @@ def compile_reads( readfilelist, outputdir ):
             files_written[f] = None
 
     return files_written
+
+class InvalidReference(Exception): pass
+
+def bwa_mem( read1, mate=None, ref=None, output='bwa.sai' ):
+    '''
+        Runs the bwa mem algorithm on read1 against ref. If mate is given then run that file with the read1 file
+        so paired alignment is done.
+
+        TODO:
+            bwa_path should be an option to specify where the executable is
+
+        @param read1 - File path to read
+        @param mate - Mate file path
+        @param ref - Reference file path
+        @param output - The output destination
+
+        @returns the output path if sucessful or -1 if something went wrong
+    '''
+    # First, make sure the reference is indexed
+    if not index_ref(ref):
+        raise InvalidReference("{} cannot be indexed by bwa")
+
+    # Setup BWA Mem
+    mem = None
+    if mate:
+        mem = BWAMem( ref, read1, mate, bwa_path=which_bwa() )
+    else:
+        mem = BWAMem( ref, read1, bwa_path=which_bwa() )
+
+    ret = mem.run( output )
+    print "Ret: " + str(ret)
+    if ret != 0:
+        return ret
+    else:
+        return output
--- tests/test_run_bwa.py
+++ tests/test_run_bwa.py
@@ -18,55 +18,68 @@ class Base(BaseClass):
         return linecount
 
 @attr('current')
-@patch('bwa.BWAMem',spec=True,run=Mock(return_value=0))
-@patch('bwa.index_ref',spec=True,return_value=True)
 class TestFunctionalRunBWA(Base):
-    def test_bwa_mem_nonpaired(self,index_ref,bwamem):
-        from run_bwa import bwa_mem
-        result = bwa_mem( 'F.fq', mate=None, ref='ref.fna' )
-        eq_( 'bwa.bam', result )
+    #@attr('current')
+    def test_bwa_mem_nonpaired(self):
+        with patch('run_bwa.BWAMem', return_value=Mock( run=Mock( return_value=0 ) ) ) as b:
+            with patch('run_bwa.index_ref',Mock(return_value=True)) as a:
+                from run_bwa import bwa_mem
+                result = bwa_mem( 'F.fq', mate=None, ref='ref.fna' )
+                eq_( 'bwa.sai', result )
 
-    def test_bwa_mem_paired(self,index_ref,bwamem):
-        from run_bwa import bwa_mem
-        result = bwa_mem( 'F.fq', mate='R.fq', ref='ref.fna' )
-        eq_( 'bwa.bam', result )
+    def test_bwa_mem_paired(self):
+        with patch('run_bwa.BWAMem', return_value=Mock( run=Mock( return_value=0 ) ) ) as b:
+            with patch('run_bwa.index_ref',Mock(return_value=True)) as a:
+                from run_bwa import bwa_mem
+                result = bwa_mem( 'F.fq', mate='R.fq', ref='ref.fna' )
+                eq_( 'bwa.sai', result )
 
-    def test_bwa_mem_output_arg(self,index_ref,bwamem):
-        from run_bwa import bwa_mem
-        result = bwa_mem( 'F.fq', mate='R.fq', ref='ref.fna', output='file.bam' )
-        eq_( 'file.bam', result )
+    def test_bwa_mem_output_arg(self):
+        with patch('run_bwa.BWAMem', return_value=Mock( run=Mock( return_value=0 ) ) ) as b:
+            with patch('run_bwa.index_ref',Mock(return_value=True)) as a:
+                from run_bwa import bwa_mem
+                result = bwa_mem( 'F.fq', mate='R.fq', ref='ref.fna', output='file.sai' )
+                eq_( 'file.sai', result )
 
-    def test_bwa_mem_fails(self,index_ref,bwamem):
-        from run_bwa import bwa_mem
-        result = bwa_mem( 'F.fq', mate='R.fq', ref='ref.fna', output='file.bam' )
-        eq_( -1, result )
+    def test_bwa_mem_fails(self):
+        with patch('run_bwa.BWAMem', return_value=Mock( run=Mock( return_value=1 ) ) ) as b:
+            with patch('run_bwa.index_ref',Mock(return_value=True)) as b:
+                from run_bwa import bwa_mem
+                result = bwa_mem( 'F.fq', mate='R.fq', ref='ref.fna', output='file.sai' )
+                eq_( 1, result )
 
-    def test_ref_index_fails(self,index_ref,bwamem):
-        from run_bwa import bwa_mem
-        result = bwa_mem( 'F.fq', mate='R.fq', ref='ref.fna', output='file.bam' )
-        eq_( -1, result )
+    @patch('run_bwa.index_ref', Mock(return_value=False))
+    def test_ref_index_fails(self):
+        from run_bwa import bwa_mem, InvalidReference
+        try:
+            bwa_mem( 'F.fq', mate='R.fq', ref='ref.fna', output='file.sai' )
+        except InvalidReference as e:
+            pass
+        else:
+            assert False, "Did not raise InvalidReference"
 
+@attr('current')
 class TestIntegrateRunBWA(Base):
     def setUp(self):
         self.read1,self.read2,self.ref = fixtures.get_sample_paired_reads()
 
     def test_maps_reads_paired(self):
         from run_bwa import bwa_mem
-        eq_( 'bwa.bam', bwa_mem( self.read1, self.read2, ref=self.ref ) )
-        assert exists( 'bwa.bam' ), "Did not create a bam file"
-        assert os.stat('bwa.bam').st_size != 0, "Bam file created is zero bytes"
+        eq_( 'bwa.sai', bwa_mem( self.read1, self.read2, ref=self.ref ) )
+        assert exists( 'bwa.sai' ), "Did not create a sai file"
+        assert os.stat('bwa.sai' ).st_size != 0, "sai file created is zero bytes"
 
     def test_maps_reads_single(self):
         from run_bwa import bwa_mem
-        eq_( 'bwa.bam', bwa_mem( self.read1, ref=self.ref ) )
-        assert exists( 'bwa.bam' ), "Did not create a bam file"
-        assert os.stat('bwa.bam').st_size != 0, "Bam file created is zero bytes"
+        eq_( 'bwa.sai', bwa_mem( self.read1, ref=self.ref ) )
+        assert exists( 'bwa.sai' ), "Did not create a sai file"
+        assert os.stat('bwa.sai').st_size != 0, "sai file created is zero bytes"
 
     def test_output_param(self):
         from run_bwa import bwa_mem
-        eq_( 'bwa.bam', bwa_mem( self.read1, ref=self.ref, output='file.bam' ) )
-        assert exists( 'file.bam' ), "Did not create a bam file"
-        assert os.stat('file.bam').st_size != 0, "Bam file created is zero bytes"
+        eq_( 'file.sai', bwa_mem( self.read1, ref=self.ref, output='file.sai' ) )
+        assert exists( 'file.sai' ), "Did not create a sai file"
+        assert os.stat('file.sai').st_size != 0, "sai file created is zero bytes"
 
 @patch('bwa.seqio.concat_files')
 class TestFunctionalCompileReads(Base):
