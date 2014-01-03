dedfb089c6b28014b47cc367c7a1f0887697abbe
Tyghe Vallard <vallardt@gmail.com>
2014-01-03 10:01:57 -0500

    Now supports indel

--- stats_at_refpos.py
+++ stats_at_refpos.py
@@ -11,7 +11,23 @@ def main(args):
     stats_at_pos( args.refpos, args.bamfile, args.minqual, args.maxd )
 
 def parse_args(args=sys.argv[1:]):
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description='''Gives stats about a given site in a bam file''',
+        epilog=u'''
+            Some things to note at this time:
+             Still confused about how the samtools manpage for mpileup says that the output should
+             be:
+                In the pileup format (without -uor-g), each line represents a genomic position, consisting of chromosome name, coordinate, reference base, read bases, read qualities and alignment mapping qualities. Information on match, mismatch, indel, strand,  mapping  quality  and
+                 start  and  end of a read are all encoded at the read base column. At this column, a dot stands for a match to the reference base on the forward strand, a comma for a match on the reverse strand, a '>' or '<' for a reference skip, 'ACGTN' for a mismatch on the forward
+                 strand and 'acgtn' for a mismatch on the reverse strand. A pattern '\+[0-9]+[ACGTNacgtn]+' indicates there is an insertion between this reference position and the next reference position. The length of the insertion is given by the integer in the pattern, followed  by
+                 the inserted sequence. Similarly, a pattern '-[0-9]+[ACGTNacgtn]+' represents a deletion from the reference. The deleted bases will be presented as '*' in the following lines. Also at the read base column, a symbol '^' marks the start of a read. The ASCII of the char-
+                 acter following '^' minus 33 gives the mapping quality. A symbol '$' marks the end of a read segment.
+            However, instead it seems you get chromosome name, coordinate, reference base, read depth, read bases, mapping quality
+            Not sure how to get read base qualities as well like it says, but that would be great
+            
+            Insertions and deletions in the read bases (+/-[0-9][a-zA-Z]+) will simply have mapping quality 0 inserted for them by this script. Good idea or not...
+        '''
+    )
 
     parser.add_argument(
         dest='bamfile',
@@ -50,6 +66,37 @@ def mpileup( bamfile, regionstr=None, minqual=25, maxd=100000 ):
     p = Popen( cmd, stdout=PIPE )
     return p.stdout
 
+def indel( sequence, pos, quallist, qualpadding=0 ):
+    ''' 
+        Returns new quallist with inserted quals
+    
+        >>> from nose.tools import eq_
+        >>> quals = [1,1,1,1]
+        >>> seq = 'abc+2aag'
+        >>> r = indel( seq, 3, quals )
+        >>> e = [1,1,1,0,0,1]
+        >>> eq_( e, r, "Insert in middle {} != {}".format(e,r) )
+        >>> seq = '+2aaAAAA'
+        >>> r = indel( seq, 0, quals )
+        >>> e = [0,0,1,1,1,1]
+        >>> eq_( e, r, "Insert at beginning {} != {}".format(e,r) )
+        >>> seq = 'AAAA+2aa'
+        >>> e = [1,1,1,1,0,0]
+        >>> r = indel( seq, 4, quals )
+        >>> eq_( e, r,"Insert at end {} != {}".format(e,r) )
+    '''
+    # Should be how many as pos is the index of the +/- in sequence
+    n = int(sequence[pos+1])
+    if sequence[pos] == '-':
+        raise ValueError( "I don't know how to do deletions yet" )
+    elif sequence[pos] == '+':
+        left = quallist[:pos]
+        right = quallist[pos:]
+        insert = [qualpadding]*n
+        quallist = left + insert + right
+
+    return quallist
+
 def stats_at_pos( refpos, bamfile, minqual, maxd ):
     base_stats = {}
     pile = mpileup( bamfile )
@@ -59,19 +106,20 @@ def stats_at_pos( refpos, bamfile, minqual, maxd ):
         depth = int(line[3])
         if pos != refpos:
             continue
-        quals = [ord(q)-33 for q in line[5]]
         bases = []
-        '''
+        quals = [ord(q)-33 for q in line[5]]
         for i in range(len(line[4])):
             b = line[4][i]
-            if b.upper() not in 'ATGC':
-                print "Skipping unknown base '{}'".format(b)
-                print "Surrounding area '{}'".format(line[4][i-4:i+4])
+            if b.upper() not in 'ATGC*':
+                #print "Skipping unknown base '{}'".format(b)
+                #print "Surrounding area '{}'".format(line[4][i-4:i+4])
+                # Do insert/delete
+                if b in '+-':
+                    quals = indel( line[4], i, quals )
+                continue
             bases.append(b)
-        '''
-        bases = [b for b in line[4] if b.upper() in 'ATGC*' ]
         bq = [x for x in itertools.izip_longest(bases,quals, fillvalue='!')]
-        assert len(quals) == len(bases), "Number of quals {} !=  number of bases {}\n".format(len(quals),len(bases))
+        assert len(quals) == len(bases), "Number of quals {} !=  number of bases {}\n{}".format(len(quals),len(bases),line)
 
         # Stats
         stats = {
@@ -88,12 +136,12 @@ def stats_at_pos( refpos, bamfile, minqual, maxd ):
 
         print "Minumum Quality Threshold: {}".format(minqual)
         print "Maximum Depth: {}".format(maxd)
-        print "Average Quality: {}".format(stats['avgqual']/stats['depth'])
+        print "Average Mapping? Quality: {}".format(stats['avgqual']/stats['depth'])
         for base, quals in stats.iteritems():
             if base not in ('depth','avgqual'):
                 print "Base: {}".format(base)
                 print "\tDepth: {}".format(len(quals))
-                print "\tAverage Quality: {}".format(float(sum(quals)) / len(quals))
+                print "\tAverage Mapping? Quality: {}".format(float(sum(quals)) / len(quals))
                 print "\t% of Total: {}".format((float(len(quals))/stats['depth'])*100)
 
         # Quit out of loop we are done
