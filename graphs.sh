f328cce20dd15ae0f4e54b49c565b8eb9c5a8b29
Tyghe Vallard <vallardt@gmail.com>
2014-02-06 08:21:48 -0500

    graphs.sh and variants.sh are no longer dependent on having miseqpipeline in a specific location

--- graphs.sh
+++ graphs.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# miseqpipeline dir
+THIS=$(cd $(dirname $0) && pwd)
+
+if [ ! -d Projects ]
+then
+    echo "No Projects directory"
+    exit 1
+fi
+
 # Ya, pretty ugly
 CPUS=$(for pid in $(awk '/physical id/ {print $4}' /proc/cpuinfo | sort | uniq); do egrep -xA 12 "processor[[:space:]]: $pid" /proc/cpuinfo; done | awk '/cpu cores/ {print $4}' | paste -sd+ | bc)
 
@@ -7,11 +16,11 @@ for p in Projects/*
 do
     if [ "$1" == "-norecreate" ]
     then
-        echo /usr/bin/time miseqpipeline/graphsample.py ${p}/$(basename $p).bam -od $p -qualdepth ${p}/$(basename $p).bam.qualdepth.json
+        echo /usr/bin/time ${THIS}/graphsample.py ${p}/$(basename $p).bam -od $p -qualdepth ${p}/$(basename $p).bam.qualdepth.json
     else
-        echo /usr/bin/time miseqpipeline/graphsample.py ${p}/$(basename $p).bam -od $p
+        echo /usr/bin/time ${THIS}/graphsample.py ${p}/$(basename $p).bam -od $p
     fi
 done | xargs -n 5 -P $CPUS -I CMD bash -c CMD
 
-miseqpipeline/graph_mapunmap.py Projects/*/*.qualdepth.json -o MapUnmapReads.png
+${THIS}/graph_mapunmap.py Projects/*/*.qualdepth.json -o MapUnmapReads.png
 convert -quality 25 -compress JPEG Projects/*/*.png QualDepth.pdf
