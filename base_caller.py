e92eb32236a95010d50506cb27d021e0d5808957
Tyghe Vallard <vallardt@gmail.com>
2014-02-12 13:19:43 -0500

    Changed caller function to utilize stats instead of bamfile/regionstr

--- base_caller.py
+++ base_caller.py
@@ -25,31 +25,43 @@ def label_N( stats, minbq ):
     stats2['mqualsum'] = stats['mqualsum']
     stats2['bqualsum'] = stats['bqualsum']
 
-
     for base, quals in stats.iteritems():
         # Only interested in base stats in this loop
         if base not in ('depth','mqualsum','bqualsum'):
-
             # generates a list called bquals
             bquals = quals['baseq']
-
             # loop to examine the quality score and identifyes bases with a quality score less than the minbq of 25
             for q in bquals:
                 k = base
                 if q < minbq:
                     k = 'N'
-
                 # adds the N to the nucleotides (A C G T and N)
                 if k not in stats2:
                     stats2[k] = {'baseq':[]}
                 stats2[k] ['baseq'].append( q )
-
-
     return stats2
 
+def generate_vcf( bamfile, regionstr, vcf_output_file, minbq, maxd, vcf_template, mind=10, minth=0.8 ):
+    '''
+        Generates a vcf file from a given vcf_template file
 
+        @param bamfile - 
 
-def caller( bamfile, regionstr, minbq, maxd, mind=10, minth=0.8 ):
+        @returns path to vcf_output_file
+    '''
+    pass
+
+def generate_vcf_row( bamfile, regionstr, minbq, maxd, mind=10, minth=0.8 ):
+    '''
+        Generates a vcf row and returns it as a string
+
+        @returns a vcf.model._Record
+    '''
+    # call the nucleotides
+    #s = stats(bamfile, regionstr, 1, 1, maxd)
+    pass
+
+def caller( stats, minbq, maxd, mind=10, minth=0.8 ):
     '''
         Calls a given base at refstr inside of bamfile. At this time refstr has to be a single
         base position('refname':N-N). The base is determined by first labeling all bases less than minbq as N and then
@@ -59,8 +71,7 @@ def caller( bamfile, regionstr, minbq, maxd, mind=10, minth=0.8 ):
 
         The final stage is to call call_on_pct with the remaining statistics
 
-        @param bamfile - Path to a bamfile
-        @param refstr - Region string acceptable for samtools
+        @param stats - stats dictionary generated from stats_at_refpos.stats
         @param minbq - Minimum base quality to determine if it is low quality or not to call it an N
         @param maxd - Maximum depth in the pileup to allow
         @param mind - Minimum depth threshold
@@ -68,12 +79,8 @@ def caller( bamfile, regionstr, minbq, maxd, mind=10, minth=0.8 ):
 
         @returns one of the items from the set( 'ATGCMRWSYKVHDBN' )
     '''
-    # call the nucleotides
-
-    s = stats(bamfile, regionstr, 1, 1, maxd)
-    stats2 = label_N(s, minbq)
-
-
+    # Label low quality as N
+    stats2 = label_N(stats, minbq)
     # if the depth is >= min depth then just remove the N's
     if stats2['depth'] >= mind:
         if 'N' in stats2:        
@@ -81,12 +88,10 @@ def caller( bamfile, regionstr, minbq, maxd, mind=10, minth=0.8 ):
     else:
         # Else we will determine if the N's are the majority now
         # defines if the base is an N
-
         if 'N' in stats2:
             np = len(stats2['N']['baseq'])/(stats2['depth']*1.0)
             if np > (1-minth):
                 return 'N'
-
     return call_on_pct(stats2, minth)
 
 def call_on_pct( stats, minth=0.8 ):
@@ -104,26 +109,13 @@ def call_on_pct( stats, minth=0.8 ):
         @returns the called base based on the percentages in the given stats
     '''
     nt_list = []
-
-
-
     for base, quals in stats.iteritems():
         # Only interested in base stats in this loop
         if base not in ('depth','mqualsum','bqualsum'):
-
             # generates a list called bquals
             bquals = quals['baseq']    
-
-
             np_2 = len(bquals)/(stats['depth']*1.0)
-
             if np_2 > round((1-minth),1): # fix for proper calculations with float
                 nt_list.append( base )
-
     dnalist = ''.join(sorted(nt_list))
-
-
-
     return iupac_amb(dnalist)
-
-
--- tests/test_base_caller.py
+++ tests/test_base_caller.py
@@ -74,26 +74,24 @@ class TestUnitLabelN(Base):
         r = self._C( self.stats, 25 )
         assert 'N' not in r, 'N was added to stats when it should not have'
 
-@patch('base_caller.stats')
 class TestUnitCaller(Base):
     def setUp( self ):
         super( TestUnitCaller, self ).setUp()
 
-    def _C( self, bamfile, refstr, minbq, maxd, mind, minth ):
+    def _C( self, stats2, minbq, maxd, mind, minth ):
         from base_caller import caller
-        return caller( bamfile, refstr, minbq, maxd, mind, minth )
+        return caller( stats2, minbq, maxd, mind, minth )
 
-    def test_no_majority( self, mock_stats ):
+    def test_no_majority( self ):
         base_stats = {
             'A': { 'baseq': [25]*76 },
             'G': { 'baseq': [25]*12 },
             'C': { 'baseq': [25]*12 }
         }
         stats = self.make_stats( base_stats )
-        mock_stats.return_value = stats
-        eq_( 'A', self._C( '', '', 25, 100000, 10, 0.8 ) )
+        eq_( 'A', self._C( stats, 25, 100000, 10, 0.8 ) )
 
-    def test_calls_multiple_ambiguous( self, mock_stats ):
+    def test_calls_multiple_ambiguous( self ):
         # 33% A, 33% G, 33% C should end up V
         base_stats = {
             'A': { 'baseq': [25]*33 },
@@ -101,38 +99,34 @@ class TestUnitCaller(Base):
             'C': { 'baseq': [25]*33 }
         }
         stats = self.make_stats( base_stats )
-        mock_stats.return_value = stats
-        eq_( 'V', self._C( '', '', 25, 100000, 100, 0.8 ) )
+        eq_( 'V', self._C( stats, 25, 100000, 100, 0.8 ) )
 
-    def test_calls_low_coverage_n( self, mock_stats ):
+    def test_calls_low_coverage_n( self ):
         # 79% A, 21% Low Quality G should end up 21% N
         base_stats = {
             'A': { 'baseq': [25]*78 },
             'G': { 'baseq': [24]*21 }
         }
         stats = self.make_stats( base_stats )
-        mock_stats.return_value = stats
-        eq_( 'N', self._C( '', '', 25, 100000, 100, 0.8 ) )
+        eq_( 'N', self._C( stats, 25, 100000, 100, 0.8 ) )
 
-    def test_calls_minth( self, mock_stats ):
+    def test_calls_minth( self ):
         # 80% A, 20% G should be called A
         base_stats = {
             'A': { 'baseq': [40]*80 },
             'G': { 'baseq': [40]*20 }
         }
         stats = self.make_stats( base_stats )
-        mock_stats.return_value = stats
-        eq_( 'A', self._C( '', '', 25, 100000, 10, 0.8 ) )
+        eq_( 'A', self._C( stats, 25, 100000, 10, 0.8 ) )
 
-    def test_calls_specific_ambiguious( self, mock_stats ):
+    def test_calls_specific_ambiguious( self ):
         # 79% A, 21% G should be called R
         base_stats = {
             'A': { 'baseq': [40]*79 },
             'G': { 'baseq': [40]*21 }
         }
         stats = self.make_stats( base_stats )
-        mock_stats.return_value = stats
-        eq_( 'R', self._C( '', '', 25, 100000, 10, 0.8 ) )
+        eq_( 'R', self._C( stats, 25, 100000, 10, 0.8 ) )
 
 class TestUnitCallOnPct(Base):
     def _C( self, stats, minth ):
