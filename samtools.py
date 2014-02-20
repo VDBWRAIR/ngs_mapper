f3a919bc15c2cefdd1a6d2a7ee09685c5f06bc8c
Tyghe Vallard <vallardt@gmail.com>
2014-02-20 08:09:45 -0500

    Lots more documentation

--- samtools.py
+++ samtools.py
@@ -3,16 +3,39 @@ import numpy as np
 import itertools
 
 def mpileup( bamfile, regionstr=None, minmq=20, minbq=25, maxd=100000 ):
+    '''
+        A simple  wrapper around the samtools mpileup command to ensure that
+        samtools mpileup is called the way we would expect for MPilupColumn to work
+
+        @param bamfile - path to a bam file
+        @param regionstr - Region string acceptable to the -r option for mpileup. If None is provided,
+            then the same output as not specifying the -r option to mpileup is expected
+        @param minmq - Minimum mapping qualty threshold. Same as -q option to mpileup
+        @param minbq - Minimum base quality or min BAQ. Same as -Q option to mpileup
+        @param maxd - Maximum depth to consider. Same as -d option to mpileup
+
+        @returns the stdout file descriptor handle
+    '''
+    # The command that will be executed
     cmd = ['samtools','mpileup','-s','-q',str(minmq),'-Q',str(minbq),'-d','{}'.format(maxd)]
+    # Only include -r if regionstr is set
     if regionstr:
         cmd += ['-r',regionstr]
+    # Add the bamfile to the last parameter
     cmd.append( bamfile )
+    # The command will be executed and output sent through the pipe so that it can be iterated
+    # over
     p = Popen( cmd, stdout=PIPE )
+    # Return the stdout file descriptor handle so it can be easily iterated
     return p.stdout
 
 def char_to_qual( qual_char ):
     '''
         Converts a given quality character to the phred - 33 integer
+        
+        @param qual_char - Quality character to convert to a phred - 33 integer quality
+
+        @return phred - 33 quality integer
     '''
     return ord( qual_char ) - 33
 
@@ -46,6 +69,7 @@ class MPileupColumn(object):
     
     @depth.setter
     def depth( self, value ):
+        ''' Depth is an integer '''
         self.__dict__['depth'] = int(value)
 
     @property
@@ -54,46 +78,75 @@ class MPileupColumn(object):
     
     @pos.setter
     def pos( self, value ):
+        ''' Position is an integer '''
         self.__dict__['pos'] = int(value)
 
     @property
     def bases( self ):
+        '''
+            Returns the bases with the inserts, deletions, $ and ^qual removed.
+            This means it returns just the bases that are really of interest.
+            it also includes the * which indicates a deletion.
+        '''
+        # Will contain the cleaned base
         cleaned = ''
         indel = False
         indel_count = None
         indel_base = ''
         for x in self._bases:
-            if x in '+-':                                                                                                                                                                                                                                                                                                            
+            # If a new insert or delete is starting
+            if x in '+-':
                 indel = x
                 indel_count = None
                 continue
+            # Only if we are parsing through an indel
             if indel != False:
+                # Parse the integer of how many bases to chomp
                 if indel in '+-' and indel_count is None:
                     indel_count = int(x)
                     continue
+                # We are on a base that needs to be chomped
                 if indel in '+-':
+                    # Chomp the base
                     if indel_count > 0:
                         indel_count -= 1
                         indel_base = x
                         continue
+                    # Last base in the series
                     else:
                         if indel == '+':
+                            # Indel bases do not have base quality nor mapping quality
+                            # so do not include them in self.bases
                             #cleaned += indel_base
                             pass
                         indel = False
                         indel_count = None
+            # Add any normal base, but make it uppercase
             if x.upper() in ('ACTGN*'):
                 cleaned += x.upper()
+            # . and , mean a match to the reference base
             elif x in ('.,'):
                 cleaned += self.refbase
         return cleaned
 
     @property
     def bquals( self ):
+        '''
+            Returns the base qualities as a phred - 33 integer
+        '''
         return map(char_to_qual, self._bquals)
 
     @property
     def mquals( self ):
+        '''
+            Returns the mapping qualities as a phred - 33 integer
+            Truncates mquals to the same length as base qualities as older samtools
+            had a bug where it would return all mapping qualities regardless of the -q and -Q 
+            thresholds being set.
+            It will return the empty list if it would otherwise truncate due to the reason above, but
+            all the values are not the same since there would be no way to tell what qual values
+            match what bases.
+        '''
         # Check to make sure map qual len is same as base qual length
         if len(self._bquals) == len(self._mquals):
             return map(char_to_qual, self._mquals)
@@ -105,20 +158,40 @@ class MPileupColumn(object):
             return []
 
     def bqual_avg( self ):
+        ''' Returns the mean of the base qualities rounded to 2 places '''
         return round( np.mean( self.bquals ), 2 )
 
     def mqual_avg( self ):
+        ''' Returns the mean of the mquals rounded to 2 places '''
         return round( np.mean( self.mquals ), 2 )
 
     def __iter__( self ):
+        '''
+            Returns an iterator that zips together bases, base qualities and mapping qualities
+            Since mquals sometimes may be missing izip_longest will fill it with 0's
+        '''
         return itertools.izip_longest( self.bases, self.bquals, self.mquals, fillvalue=0 )
 
     def base_stats( self ):
+        '''
+            Returns a compiled statistics dictionary for all the different base values in this column
+            The dictionary must contain the following keys:
+                depth: Total depth of this column which should be self.depth
+                bqualsum: sum of the base qualities
+                mqualsum: sum of the mapping qualities
+                'A/C/T/G/N/*': dictionary of information about the mapping and base qualities for an individual base
+                    mapq: list of all the mapping qualities for this base(int(phred - 33))
+                    baseq: list of all the base qualities for this base(int(phred - 333))
+
+            @returns the stats dictionary
+        '''
         bquals = self.bquals
         mquals = self.mquals
         bqualsum = float( sum( bquals ) )
         mqualsum = float( sum( mquals ) )
-        assert len(bquals) == len(mquals), "Somehow length of Base Qualities != Map Qualities"
+        # Lets just make sure of a few things because samtools mpileup isn't exactly documented the best
+        assert len(bquals) == len(mquals), "Somehow length of Base Qualities != length of Map Qualities"
+        assert len(bquals) == self.depth, "Somehow length of bases != length of Base Qualities"
         depth = self.depth
         stats = {'depth':depth,'mqualsum':mqualsum,'bqualsum':bqualsum}
         for b,bq,mq in self:
@@ -129,4 +202,5 @@ class MPileupColumn(object):
         return stats
 
     def __str__( self ):
+        ''' Returns the mpileup string '''
         return "{ref}\t{pos}\t{refbase}\t{depth}\t{_bases}\t{_bquals}\t{_mquals}".format(**self.__dict__)
