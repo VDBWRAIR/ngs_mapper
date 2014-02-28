4d79142b200df81f36af99ca80a630a5ceba1ec7
Tyghe Vallard <vallardt@gmail.com>
2014-02-28 09:52:51 -0500

    No longer going to manage permissions as it just causes problems

--- runsamplesheet.sh
+++ runsamplesheet.sh
@@ -27,12 +27,12 @@ then
     exit 1
 fi
 
-if [ ! -e ${scripts}/perms.sh ]
-then
-    echo "Please setup perms.sh first"
-    echo "You can copy perms.sh-example to perms.sh and then modify it"
-    exit 1
-fi
+#if [ ! -e ${scripts}/perms.sh ]
+#then
+#    echo "Please setup perms.sh first"
+#    echo "You can copy perms.sh-example to perms.sh and then modify it"
+#    exit 1
+#fi
 
 # Call perms.sh to setup the permissions environment
 bash ${scripts}/perms.sh
