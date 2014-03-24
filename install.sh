1ae8718fad4b5fa30b08a36971f033d20faf2646
Tyghe Vallard <vallardt@gmail.com>
2014-03-24 08:45:17 -0400

    Fixes #262. Checks for development libraries that are needed

--- install.sh
+++ install.sh
@@ -32,10 +32,10 @@ function pyinstall() {
 }
 
 # Ensure correct python version
-python -V 2>&1 | grep -qE '2.7.[0-9]'
+python -V 2>&1 | grep -qE '2.7.[3456789]'
 if [ $? -ne 0 ]
 then
-    echo "Please ensure you have python 2.7 or greater"
+    echo "Please ensure you have python 2.7.3 or greater but less than 3"
     exit 1
 fi
 
@@ -48,17 +48,16 @@ then
     exit 1
 fi
 
-# Ensure zlib.h is in include path(I guess we won't assume redhat here and just do rpm -qa stuff)
-if [ -z "$(find $(echo | cpp -x c++ -Wp,-v 2>&1 | grep -v 'ignoring' | grep -v '^#' | grep -v '^End' | xargs) -type f -name zlib.h)" ]
-then
-    echo "Please ensure that the zlib development package is installed and then rerun this installer."
-    echo "Red Hat: yum install -y zlib-devel"
-    echo "Ubuntu: apt-get install -y zlib-devel"
-    exit 1
-fi
-
-# If any command exits then trap it and print error and exit
-trap 'echo "Error running $BASH_COMMAND"; rm -rf man1; exit;' ERR SIGINT SIGTERM
+INCLUDE=$(echo | cpp -x c++ -Wp,-v 2>&1 | grep -v 'ignoring' | grep -v '^#' | grep -v '^End' | xargs)
+for dev in zlib.h png.h curses.h
+do
+    # Ensure dev is in include path
+    if [ -z "$(find ${INCLUDE} -type f -name ${dev})" ]
+    then
+        echo "Please ensure that the package that provides $dev is installed and then rerun this installer."
+        exit 1
+    fi
+done
 
 # Create the virtual environment where everything will install to
 # Don't use setuptools as we will install that later
@@ -113,7 +112,7 @@ do
     fi
 done
 
-# Install all python packages
+# Install all python packages(the ordering is important because of dependencies on each other)
 package_list=( setuptools PyVCF numpy nose pyparsing tornado six python-dateutil matplotlib biopython pyBWA mock )
 cd ${deppath}
 for package in ${package_list[@]}
