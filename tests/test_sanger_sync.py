b6016df0aca26a1a5908c920cf05f602c2aa9c65
Tyghe Vallard <vallardt@gmail.com>
2014-05-28 12:40:45 -0400

    Still in progress. Working on link_reads now

--- tests/test_sanger_sync.py
+++ tests/test_sanger_sync.py
@@ -1,10 +1,25 @@
 from imports import *
 import common
+import string
 
 class Base( common.BaseClass ):
     sampleabi = join( fixtures.FIXDIR, 'sample.ab1' )
     rund = 'Run_3130xl_{year}-{month}-{day}_01-01_{run}_{year}-{month}-{day}'
     sample = '{sample}_{primer}_{year}_{month}_{day}_{virus}_{gene}_{run}_{well}.{ext}'
+
+    def _syncrun( self, ngsdata ):
+        from sanger_sync import sync_run
+        rund, samples, abilist = self.make_from_dir()
+        r = sync_run( rund, ngsdata )
+        rawd = join( ngsdata, 'RawData', 'Sanger', rund )
+        return r, abilist, rund, rawd
+
+    def _syncrunread( self, ngsdata ):
+        from sanger_sync import sync_readdata
+        r, abilist, rund, rawd = self._syncrun( ngsdata )
+        sync_readdata( rawd, ngsdata )
+        readdata = join( ngsdata, 'ReadData', 'Sanger', rund )
+        return r, abilist, rund, rawd, readdata
 
     def make_from_dir( self, path=None ):
         if path is None:
@@ -19,9 +34,9 @@ class Base( common.BaseClass ):
 
         abilist = []
         for sample in samples:
-            sample['year'] = 2001
-            sample['month'] = 01
-            sample['day'] = 01
+            sample['year'] = '2001'
+            sample['month'] = '01'
+            sample['day'] = '01'
             filename = self.sample.format( **sample )
             abilist.append( filename )
             filepath = join( path, filename )
@@ -38,14 +53,19 @@ class Base( common.BaseClass ):
 
     def check_readdata( self, abilist, readd ):
         ''' abilist is just basename list of abi files expected '''
-        abi = [basename(r) for r in glob.glob( join( readd, '*.ab1' ) )]
+        abi = [basename(r) for r in glob( join( readd, '*.ab1' ) )]
         abi = set( abi )
         abilist_set = set( abilist )
         eq_( set([]), abilist_set - abi, 'Abi lists were not identical {} != {}'.format(abi, abilist_set) )
         for abi in abilist:
-            ok( islink( abi ), '{} is not a valid file/symlink'.format(abi) )
-            j = join( readd, abi.replace('.ab1', '.fastq') )
-            ok_( islink( j ), 'Fastq version of {} does not exist'.format(abi) )
+            a = join( readd, abi )
+            ok_( islink( a ), '{} is not a valid file/symlink'.format(a) )
+            j = a.replace('.ab1', '.fastq')
+            self.print_ngs()
+            ok_( isfile( j ), 'Fastq version of {} does not exist'.format(j) )
+
+    def print_ngs( self ):
+        print subprocess.check_output( 'find .', shell=True )
 
     def check_rbs( self, abilist, rbs ):
         ''' abilist is just basename list of abi files expected '''
@@ -70,15 +90,109 @@ class TestSync( Base ):
         self.check_rawdata( abilist, join(ngsdata,'ReadData','Sanger',rund) )
         self.check_rbs( abilist, join(ngsdata,'ReadsBySample') )
 
+class TestSampleNameFromRead( Base ):
+    def _C( self, *args, **kwargs ):
+        from sanger_sync import samplename_from_read
+        return samplename_from_read( *args, **kwargs )
+
+    def test_valid_samplename( self ):
+        validnames = [
+            'sample1',
+            'sample-1',
+            'sample_1',
+        ]
+        for name in validnames:
+            for p in 'FR':
+                sn = self.sample.format(
+                    sample=name, primer=p+'123',
+                    year='2001', month='01', day='01',
+                    virus='Vir1', gene='Gen1',
+                    run='0001', well='A01', ext='fastq'
+                )
+                r = self._C( sn )
+                eq_( name, r )
+
+    def test_invalid_samplename( self ):
+        from sanger_sync import InvalidFormat
+        for name, p in (('sample 1', 'F123'), ('sample 1', 'F123')):
+            sn = self.sample.format(
+                sample=name, primer=p,
+                year='2001', month='01', day='01',
+                virus='Vir1', gene='Gen1',
+                run='0001', well='A01', ext='fastq'
+            )
+            try:
+                self._C( sn )
+                ok_( False, 'Did not raise InvalidFormat exception' )
+            except InvalidFormat as e:
+                ok_( True )
+
+class TestLinkReads( Base ):
+    def _C( self, *args, **kwargs ):
+        from sanger_sync import link_reads
+        return link_reads( *args, **kwargs )
+
+    def test_syncs_non_existing( self ):
+        ngsdata = 'NGSData'
+        reads, abilist, rund, rawd, readd = self._syncrunread( ngsdata )
+        self._C( readd, ngsdata )
+        rbs = join( 'NGSData', 'ReadsBySample' )
+        self.check_rbs( abilist, rbs )
+
+    def test_syncs_existing( self ):
+        ngsdata = 'NGSData'
+        reads, abilist, rund, rawd, readd = self._syncrunread( ngsdata )
+        self._C( readd, ngsdata )
+        rbs = join( 'NGSData', 'ReadsBySample' )
+        
+        self.check_rbs( abilist, rbs )
+
 class TestSyncRead( Base ):
     def _C( self, *args, **kwargs ):
         from sanger_sync import sync_readdata
         return sync_readdata( *args, **kwargs )
 
+    def test_syncs_non_existing( self ):
+        ngsdata = 'NGSData'
+        reads, abilist, rund, rawd = self._syncrun( ngsdata )
+        readd = join( ngsdata, 'ReadData', 'Sanger', rund )
+        self._C( rawd, ngsdata )
+        self.check_readdata( abilist, readd )
+
+    def test_syncs_existing( self ):
+        ngsdata = 'NGSData'
+        reads, abilist, rund, rawd = self._syncrun( ngsdata )
+        readd = join( ngsdata, 'ReadData', 'Sanger', rund )
+        self._C( rawd, ngsdata )
+        self.check_readdata( abilist, readd )
+        # Remove file and try again
+        abi = glob( join(readd,'*.ab1') )[0]
+        fastq = glob( join(readd,'*.fastq') )[-1]
+        os.unlink( abi )
+        os.unlink( fastq )
+        self._C( rawd, ngsdata )
+        self.check_readdata( abilist, readd )
+
 class TestSyncRun( Base ):
     def _C( self, *args, **kwargs ):
         from sanger_sync import sync_run
         return sync_run( *args, **kwargs )
+
+    def test_invalid_filename( self ):
+        from sanger_sync import InvalidFormat
+        rund, samples, abilist = self.make_from_dir()
+        seqfile = self.sample.format( 
+            sample='sample 1', primer='F1', year=2001,
+            month=01, day=01, virus='Den1',
+            gene='Den1', run=0001, well='H01', ext='ab1'
+        )
+        seqf = join( rund, seqfile )
+        touch( seqf )
+        try:
+            self._C( rund, 'NGSData' )
+            ok_( False, 'Did not raise InvalidFormat' )
+        except InvalidFormat as e:
+            ok_( True )
 
     def test_syncs_run_no_dir( self ):
         rund, samples, abilist = self.make_from_dir()
@@ -114,3 +228,11 @@ class TestSyncRun( Base ):
         eq_( s.st_ctime, s2.st_ctime )
         eq_( s.st_atime, s2.st_atime )
         eq_( s.st_mtime, s2.st_mtime )
+
+    def test_check_returnvalue( self ):
+        rund, samples, abilist = self.make_from_dir()
+        rawd = join( 'NGSData', 'RawData', 'Sanger', rund )
+        r = self._C( rund, 'NGSData' )
+        expect = set([join(rawd,abi) for abi in abilist])
+        result = set(r)
+        eq_( set([]), expect ^ result, 'Return value {} != {} Diff: {}'.format(result,expect,expect ^ result) )
