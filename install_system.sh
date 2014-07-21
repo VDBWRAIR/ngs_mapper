72c14604ae380cd785f1959b4d397804a9db9d08
Tyghe Vallard <vallardt@gmail.com>
2014-07-21 14:09:56 -0400

    Addresses #7883

---
+++ install_system.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+function ensure_pkg {
+    if ! rpm -qa | grep -q "^${1}"
+    then
+        echo "Installing $1"
+        yum install -y $1
+    fi
+}
+
+function ensure_python {
+    prefix=$1
+
+    if [[ $(python -V 2>&1) =~ 2.7.[3456789] ]]
+    then
+        echo "Required version of python is already installed" 
+        return 0
+    else
+        echo "Installing required version of python"
+        wget --no-check-certificate https://www.python.org/ftp/python/2.7.3/Python-2.7.3.tgz -O- | tar xzf - && pushd Python-2.7.3 && ./configure --prefix $prefix && make && make install && popd
+        return $?
+    fi
+}
+
+function install_pipeline {
+    from=$1
+    to=$2
+
+    if [[ $from =~ .git$ ]] || [ -d ${from}/.git ]
+    then
+        echo "Installing pipeline into $to"
+        cd $to
+        pth=$(basename $from)
+        pth=${pth/.git//}
+        # Make sure no virtualenvironments are activated
+        deactivate 1>/dev/null 2>&1
+        if [ -d $pth ]
+        then
+            echo "Pipeline seems to already be installed at ${to}/${pth} so we will try to update"
+            # Update and uninstall
+            cd $pth
+            # I guess stash will work for now in case there were changes(don't want to destroy something)
+            git stash
+            git pull
+            ./uninstall.sh
+        else
+            git clone $from
+            cd $pth
+        fi
+
+        # Ensure virtualenv is installed
+        install_virtualenv ${to}/${pth}
+
+        # Install
+        ./install.sh
+        _INSTALL_PATH=${to}/${pth}
+        return $?
+    else
+        echo "$from is not a git repo"
+        return 1
+    fi
+}
+
+function install_virtualenv {
+    pipeline_install_location=$1
+    python -c "virtualenv" 2>/dev/null || pushd ${pipeline_install_location}/dependencies/virtualenv* && $(dirname $pipeline_install_location)/bin/python setup.py install && popd
+}
+
+function install_system_packages {
+    pkg_lst=$1
+    echo "Ensuring all system package dependencies are installed"
+    for pkg in ${pkg_lst[@]}
+    do
+        if ! ensure_pkg $pkg
+        then
+            return 1
+        fi
+    done
+    return 0
+}
+
+function main {
+    repo_pth=$1
+    install_pth=$2
+    pkg_lst=( libpng{,-devel} ncurses{,-devel} ImageMagick zlib{,-devel} freetype{,-devel} readline{,-devel} openssl{,-devel} gcc-c++ git )
+
+    if [ ! $(id -u) -eq 0 ]
+    then
+        echo "Please run as root"
+        return 1
+    fi
+
+    if [ -z $repo_pth ]
+    then
+        echo "Please specify the location of the miseqpipeline git repository"
+        return 1
+    fi
+
+    install_system_packages $pkg_lst && ensure_python $install_pth && install_pipeline $repo_pth $install_pth #&& install_virtualenv $install_pth
+    RET=$?
+
+    if [ $RET -eq 0 ]
+    then
+        echo "The miseqpipeline is now installed into $install_path. You can activate the pipeline by issuing the following command:"
+        echo ". ${_INSTALL_PATH}/setup"
+        return $RET
+    else
+        echo "There was an error during the installation."
+        return $RET
+    fi
+
+}
+
+main $@
