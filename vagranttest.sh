c8b7730a760e46834fe02c2e9ad425d93d377bb4
Tyghe Vallard <vallardt@gmail.com>
2014-10-06 13:09:43 -0400

    Vagrant testing wrapper script

---
+++ vagranttest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Simple script to test a given vagrant box
+
+if [ -z "$1" ]
+then
+    echo "Must supply a boxname listed within the vagrant status command output"
+    exit 1
+fi
+
+if vagrant status $1 | grep -q 'running'
+then
+    vagrant destroy $1
+fi
+
+# Bring up box. Provision and run tests
+vagrant up $1
+vagrant ssh $1 -c ". ~/.miseqpipeline/bin/activate; cd miseqpipeline; nosetests miseqpipeline"
