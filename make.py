ef9400457d3895e78f78265b0d3c82563bb91f26
averagehat <michael.panciera.work@gmail.com>
2015-10-30 12:52:05 -0400

    added commandline interface to make.py

--- make.py
+++ make.py
@@ -1,12 +1,17 @@
 from toolz.dicttoolz import valmap
 import jinja2
-import os
+import os, sys
 import yaml
 from functools import partial
-# write it to the sample outdir 
+# write it to the sample outdir
+default_args= {
+    'prefix' : 947,
+    'reference' : '947.ref.fasta'
+}
+
 
 #@typecheck(int, str, str)
-def make_template(template, dest, args, config='config.yaml'):
+def make_template(template, dest, args=default_args, config='config.yaml'):
     env = jinja2.Environment(loader=jinja2.loaders.FileSystemLoader(os.path.dirname(template)))
     plate = env.get_template(template)
     settings = get_settings(config, args)
@@ -14,16 +19,16 @@ def make_template(template, dest, args, config='config.yaml'):
     string = plate.render(settings)
     with open(dest, 'w') as out:
         out.write(string)
-    return dest 
+    return dest
 
 # Extract all the "default" values
 def get_settings(confpath, args):
     cfg = yaml.load(open(confpath))
     _ = cfg.pop('NGSDATA')
-    conf = valmap(partial(valmap, lambda x: x['default']), cfg) 
+    conf = valmap(partial(valmap, lambda x: x['default']), cfg)
     conf['args'] = {}
     # have to change the argparse destination to match
-    # overwrite with args from command-line 
+    # overwrite with args from command-line
     for argname, argval in args.__dict__.items():
         for cmd, option in conf.items():
         #this will work because the args are already over-written by the defaults in config.yaml if they were not suppplied. otherwise this would over-write with null values.
@@ -34,9 +39,17 @@ def get_settings(confpath, args):
                 assert not (argname in conf.values())
                 conf['args'][argname] = argval
     return conf
-    
 
-# Jinja won't error out if a key doesn't exist if it doesn't have a dot call . . . 
+usage = "<template> <dest>"
+
+if __name__ == '__main__':
+    try:
+        make_template(*sys.arv[1:])
+    except Exception, e:
+        print "Usage: " + usage
+
+
+# Jinja won't error out if a key doesn't exist if it doesn't have a dot call . . .
 
 
 #    def update_cmd(cmd, field): cfg.yaml[cmd][field] =  getattr(args, field)
