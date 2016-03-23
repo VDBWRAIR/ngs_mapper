04332fe217d61f944fa2009d818600944c5d0ece
Panciera <michael.panciera@amedrhcwrair001.amed.ds.army.mil>
2016-03-23 12:53:31 -0400

    added generit format converter

--- ngs_mapper/sff_to_fastq.py
+++ ngs_mapper/sff_to_fastq.py
@@ -1,11 +1,22 @@
 from ngs_mapper.reads import sffs_to_fastq
 import os, sys
 from functools import partial
+from glob import glob
+import gzip
+from Bio import SeqIO
 
+{
+    'sff' : 'sff',
+    'ab1' : 'abi',  # SeqIO.convert(x, 'abi', y, 'fastq')
+}
 
-def run(sff_paths):
-    newext = lambda s: '.'.join(s.split('.')[:-1] + ['fastq'])
-    outnames = map(newext, sff_paths)
+drop_ext = lambda s: '.'.join(s.split('.')[:-1])
+swap_ext = lambda ext: lambda s:  drop_ext(s) + '.' + ext
+find_ext = lambda ext: lambda dir: glob("%s/*%s" % (dir, ext))
+
+def convert_sff(dir):
+    sff_paths = find_ext('sff')(dir)
+    outnames = map(swap_ext('fastq'), sff_paths)
     def wrapped_conv(a, b):
         print "Converting %s to fastq into %s" % (a, b)
         n = 0
@@ -17,18 +28,40 @@ def run(sff_paths):
         return n
     return sum(map(wrapped_conv, sff_paths, outnames))
 
-def main():
+def convert_ab1(dir):
+    for abi in find_ext('gz')(dir):
+        SeqIO.convert(abi, 'abi', swap_ext('fastq')(abi), 'fastq')
+
+def convert_gzips(dir):
+    for gz in find_ext('gz')(dir):
+        dest = drop_ext(gz)
+        with gzip.open( gz, 'rb' ) as input:
+            with open(dest, 'w') as output:
+                output.write(input.read())
+
+def convert_formats(dir):
+    convert_gzips(dir)
+    convert_ab1(dir)
+    convert_sff(dir)
+
+def get_dir_arg():
     if os.path.isdir(sys.argv[1]):
-        dir = sys.argv[1]
-        sff_names = filter(lambda x: x.endswith('sff'), os.listdir(dir))
-        sff_paths = map(partial(os.path.join, dir), sff_names)
-        run(sff_paths)
+        return sys.argv[1]
     else:
-        run(sys.argv[1:])
+        raise ValueError("Path %s is not a directory" % sys.argv[1])
+
+def main_convert_formats():
+    convert_formats(get_dir_arg())
 
 
+def main_sff_convert():
+        convert_sff(get_dir_arg())
 
-if __name__ == '__main__': main()
+#        sff_names = filter(lambda x: x.endswith('sff'), os.listdir(dir))
+#        sff_paths = map(partial(os.path.join, dir), sff_names)
+#        run(sff_paths)
+
+
 
 
 
