fd3dbe0c698d3e689692305d95d9a53560808820
Panciera <michael.panciera@amedrhcwrair001.amed.ds.army.mil>
2016-03-23 14:59:46 -0400

    forgot to roll back filter test

--- ngs_mapper/tests/test_filter.py
+++ ngs_mapper/tests/test_filter.py
@@ -75,9 +75,9 @@ class TestNGSFilter(unittest.TestCase):
 
     def test_stat_file_none_filtered(self):
         write_filtered(self.inputfn, 0, False, outdir=self.outdir)
-        expected = "Index Quality was 0 and dropNs was set to False, so file %s was copied" % self.inputfn
+        expected = '''ngs_filter found %s reads in file %s, and filtered out %s reads.''' % (4, self.inputfn, 0)
         actual = open(self.statsfile).readlines()[0].strip()
-        self.assertTrue(actual.startswith(expected))
+        self.assertEquals(actual, expected)
 
     def test_stat_file_two_filtered(self):
         write_post_filter(self.inputdir, 32, True, ['Sanger'], self.outdir)
