b5291ad2fa84af0268c1db0f51043436b2757374
Tyghe Vallard <tyghe.vallard@target.com>
2017-10-04 20:40:49 -0500

    remove file

--- ngs_mapper/tests/docker_test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#docker build -t ngs_mapper:1.5 .
-docker run -it -v /Users/z0023fp/Projects/necrolyte2/ngs_mapper/NGSData:/NGSDATA ngs_mapper:1.5 ngs_mapper/tests/slow_tests.sh
